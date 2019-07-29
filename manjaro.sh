d6cf73597686aa931b675ac957b80a497020591d
Rafael Raposo <orapouso@gmail.com>
2019-07-29 16:42:58 -0300

    majaro updates

--- manjaro.sh
+++ manjaro.sh
@@ -5,35 +5,36 @@ yaourt -S --noconfirm \
 	chromium \
 	vim \
 	variety \
-	arandr \
 	vlc \
 	easytag \
-	mpd \
 	base-devel \
-	htop \
-	franz \
-	network-manager
+	htop
 
-# desktop
+# i3 desktop
+#yaourt -S --noconfirm \
+#	i3 \
+#	rxvt-unicode \
+#	feh \
+#	rofi \
+#	compton \
+#	playerctl \
+#	mpd \
+#	arandr \
+#	network-manager \
+#	xss-lock \
+#	xorg-xbacklight \
+#	polybar \
+#	ttf-font-awesome
+
 yaourt -S --noconfirm \
-	i3 \
-	rxvt-unicode \
 	zsh \
-	feh \
-	rofi \
-	compton \
-	playerctl \
-	xss-lock \
-	xorg-xbacklight \
-	polybar \
-	ttf-font-awesome
+	fonts-powerline
 
 # develop
 yaourt -S --noconfirm \
 	code \
 	gitg \
 	meld \
-	python-pip \
 	mysql-workbench \
 	docker \
 	nvm \
@@ -41,19 +42,29 @@ yaourt -S --noconfirm \
 
 # extra
 yaourt -S --noconfirm \
-	spotify
+	spotify \
+	franz
+
+# miniconda
+wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O /tmp/Miniconda3-latest-Linux-x86_64.sh
+bash /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/.miniconda
+conda init -q zsh
+conda config --set changeps1 False
+
+#oh-my-zsh shell
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # zgen
-git clone https://github.com/tarjoilija/zgen.git .zgen
+git clone https://github.com/tarjoilija/zgen.git $HOME/.zgen
 
 # config files
 git clone git@github.com:orapouso/dotfiles.git $HOME
 
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.Xresources ~/.Xresources
-ln -sf ~/dotfiles/i3/config ~/.config/i3/config
-mkdir ~/.config/polybar
-ln -sf ~/dotfiles/polybar/config ~/.config/polybar/config
-ln -sf ~/dotfiles/variety/variety.conf ~/.config/variety/variety.conf
+ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+#ln -sf $HOME/dotfiles/.Xresources $HOME/.Xresources
+#ln -sf $HOME/dotfiles/i3/config $HOME/.config/i3/config
+#mkdir $HOME/.config/polybar
+#ln -sf $HOME/dotfiles/polybar/config $HOME/.config/polybar/config
+ln -sf $HOME/dotfiles/variety/variety.conf $HOME/.config/variety/variety.conf
 
 	
